5351ebe4a16274428cdbd705f4798b42356142a2
Taloumtzis Theodoros <taloumtzistheodoros@gmail.com>
2025-04-09 14:24:17 +0300

    Add utility scripts for model handling, callbacks, and plotting
    - Implemented `model_utils.py` for saving Keras models with format handling and overwrite options.
    - Created `callbacks.py` to define custom Keras callbacks for logging losses, early stopping, and model checkpointing.
    - Added `netron_embed.py` to facilitate viewing models using Netron in Jupyter notebooks.
    - Introduced `nn_utils.py` with various utility functions for model training, including custom callbacks for saving top models and plotting training history.
    - Developed `plotting.py` for visualizing confusion matrices and ROC curves, enhancing model evaluation capabilities.

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .callbacks import all_callbacks
+from .model_utils import save_model
+from .nn_utils import plot_history, save_model, load_model, save_history, load_history, absorb_batchNorm, set_seed, get_best_ckpt, get_schedule
+from .netron_embed import view_model
+from .plotting import print_dict, plot_confusion_matrix, plotRoc, rocData, makeRoc
